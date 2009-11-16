f09ca006038de994b1dd1d087a54d312c2bfeb1c
iyatomi takehiro <iyatomi@gmail.com>
2009-11-17 01:26:22 +0900

    nbr/core/clst.h clst.c : add to repository

---
+++ nbr/core/clst.h
@@ -0,0 +1,26 @@
+/***************************************************************
+ * node.h : control master / application cluster
+ * 2009/10/27 iyatomi : create
+ *                             Copyright (C) 2008-2009 Takehiro Iyatomi
+ * This file is part of libnbr.
+ * libnbr is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either
+ * version 2.1 of the License or any later version.
+ * libnbr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of
+ * the GNU Lesser General Public License along with libnbr;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ ****************************************************************/
+#if !defined(__CLST_H__)
+#define __CLST_H__
+
+int nbr_clst_init();
+void nbr_clst_poll();
+void nbr_clst_fin();
+
+#endif
